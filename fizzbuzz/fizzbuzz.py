02f0a277f0f5b3ef5d15c3f31a68d5524ed3f40e
abesamt <abesamt@kali-abesamt.localdomain>
2020-09-30 18:36:33 -0500

    Passed test

--- fizzbuzz/fizzbuzz.py
+++ fizzbuzz/fizzbuzz.py
@@ -4,18 +4,28 @@ import sys
 
 def is_positive_integer(n):
     """Returns True if n is a positive integer"""
-
+    if type(n) is int and n > 0:
+        return True
 
     return False
 
 
 def process(n):
     """Returns the string representation of integer n unless:
-       - n is evenly divisible by 3 (returns 'Fizz')
+       - n is evenly divisible by 3 (returns 'Fizz') 
        - n is evenly divisible by 5 (returns 'Buzz')
        - n is evenly divisible by both 3 and 5 (returns 'FizzBuzz')"""
 
-    return ''
+    if n % 3 == 0 and n % 5 == 0:
+        return "FizzBuzz"
+
+    if n % 3 == 0:
+        return "Fizz"
+
+    if n % 5== 0:
+        return "Buzz"
+
+    return str(n)
 
 
 @click.command()
